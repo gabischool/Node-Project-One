96f165a9f62f356a3dd4d9336683be67f58d1790
yaasiinaxmed <yaskassoy@gmail.com>
2023-09-04 10:00:19 +0300

    i finished assigment

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // import your node server here
 import app from './api/server.js';
 
+const hostname = 'localhost'
 const port = 9000;
 
 // START YOUR SERVER HERE
-
+app.listen(port, () => console.log(`Listening on port http://${hostname}:${port}/`))
