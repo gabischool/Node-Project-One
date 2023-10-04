52b5e2e0e37086d7e9029f498f2f672bda0c2751
Abdishakuur Hassan <abdishakuurh366@gmail.com>
2023-10-04 06:09:05 -0700

    Assignment one commit one

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // import your node server here
 import app from './api/server.js';
 
+
 const port = 9000;
 
 // START YOUR SERVER HERE
 
+//server.listen(9000,()=>console.log("listening port 9000"))
